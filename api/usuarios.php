fb2d1e4d949a6e6043eeed51a6e8983be81401c1
Alvaro Schneider <alvaro.c.schneider@gmail.com>
2025-07-25 18:34:25 -0300

    agrego un cambio en conexion a db

--- api/usuarios.php
+++ api/usuarios.php
@@ -1,5 +1,6 @@
 <?php
-$conexion = new mysqli("localhost", "phpmyadmin", "sole0504", "fotomascotas");
+$conexion = new mysqli("localhost", "adminphp", "adminphp", "fotomascotas");
+//$conexion = new mysqli("localhost", "phpmyadmin", "sole0504", "fotomascotas");
 $consulta = $conexion->query("SELECT nombre, usuario FROM usuarios");
 $usuarios = [];
 while ($fila = $consulta->fetch_assoc()) {
